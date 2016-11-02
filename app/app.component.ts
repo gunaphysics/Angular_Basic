3d54a3fe34273f267128e89b4ff8157f53df3e10
guna <gunasekaran_p@homedepot.com>
2016-11-02 09:48:12 -0400

    Basic working app

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by GXP8655 on 11/2/2016.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent { }
